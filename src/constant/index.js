8515b553698d91871bdacb24e4d0ae861b199ed5
burner0621 <cutetiger0621@gmail.com>
2023-10-02 19:45:32 +0900

    twitter 5

--- src/constant/index.js
+++ src/constant/index.js
@@ -11,17 +11,17 @@ export const METAINFO = {
     HOME: {
         "title": "Cipher Labs",
         "description": "Crafting tools & analytics, providing elite solutions for DeFi trading.",
-        "twitter": "/assets/twitter-home.jpg"
+        "twitter": "/assets/opengraph-image.jpg"
     },
     DYDX: {
         "title": "Cipher Labs | dYdX", 
         "description": "Explore Cipher Labs' dedicated suite for dYdX. From an intuitive Trading Journal to powerful Analytics Tools, we provide the edge every DeFi trader seeks.",
-        "twitter": "/assets/twitter-home.jpg"
+        "twitter": "/assets/opengraph-image.jpg"
     },
     ABOUTUS: {
         "title": "Cipher Labs | About Us",
         "description": "Discover Cipher Labs: Dedicated craftsmen revolutionizing the DeFi landscape. Harnessing Web3, our mission is to empower traders with state-of-the-art tools and bespoke software.",
-        "twitter": "/assets/twitter-home.jpg"
+        "twitter": "/assets/opengraph-image.jpg"
     },
     ANALYTICS: {
         'title': 'dYdX v3 | Volume Analytics Dashboard',
